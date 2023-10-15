7b35c5396d225c02c51cac73752c007fbb4250be
Matthew <adetoyesesamson@gmail.com>
2023-10-15 05:25:35 +0100

    API implemented

--- new-app/src/Components/WeatherApp/WeatherApp.js
+++ new-app/src/Components/WeatherApp/WeatherApp.js
@@ -1,20 +1,46 @@
 import React from 'react'
 import './WeatherApp.css'
 
-import icon_sol from '../Assets/user-solid.svg'
-import weather from '../Assets/lock-solid.svg'
-import humidity from '../Assets/envelope-solid.svg'
+import searchimg from '../Assets/magnifying-glass-solid.svg'
+import sun from '../Assets/sun-solid.svg'
+import wind from '../Assets/wind-solid.svg'
+import rain from '../Assets/cloud-rain-solid.svg'
+import cloud from '../Assets/cloud-sun-solid1.svg'
+import humidity from '../Assets/water-solid.svg'
+
 function WeatherApp() {
+    let api_key = '75302c1ff56a65c59ed58e75145de771'; 
+
+    const search = async () => {
+        const element = document.getElementsByClassName('cityInput')
+        if (element[0].value === '')
+        {
+            return 0;
+        }
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+        let response = await fetch(url);
+        let data = await response.json();
+        const location = document.getElementsByClassName('weather-location')
+        const temperature = document.getElementsByClassName('weather-temp')
+        const humidity = document.getElementsByClassName('humidity-percent')
+        const wind = document.getElementsByClassName('wind-rate')
+
+        location[0].innerHTML = data.name
+        temperature[0].innerHTML = data.main.temp+'<sup>0</sup>C'
+        humidity[0].innerHTML = data.main.humidity+'%'
+        wind[0].innerHTML = data.wind.speed+'km/h'
+    }
   return (
       <div className='container'>
           <div className="top-bar">
               <input type="text" className="cityInput" placeholder='Search' />
-              <div className="search-icon">
-                  <img src={icon_sol} alt=""/>
+              <div className="search-icon" onClick={()=>{search()}}>
+                  <img src={searchimg} alt=""/>
               </div>
           </div>  
           <div className='weather-image'>
-              <img src={weather} alt=''/>
+              <img src={cloud} alt=''/>
           </div>
           <div className='weather-temp'>24<sup>o</sup>c</div>
           <div className="weather-location">London</div>
@@ -22,14 +48,14 @@ function WeatherApp() {
               <div className="element">
                   <img src={humidity} alt='' className='icon' />
                   <div className="data">
-                      <div className="humidy-percent">64%</div>
+                      <div className="humidity-percent">64%</div>
                       <div className="text">Humidity</div>
                 </div>
               </div>
               <div className="element">
-                  <img src={humidity} alt='' className='icon' />
+                  <img src={wind} alt='' className='icon' />
                   <div className="data">
-                      <div className="humidy-percent">16 km/h</div>
+                      <div className="wind-rate">16 km/h</div>
                       <div className="text">Wind Speed</div>
                   </div>
               </div> 
